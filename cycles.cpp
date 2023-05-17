de633890967199cae96c5fadb90d6111900360f3
jelo-git <batogfilip@gmail.com>
2023-05-17 18:50:28 +0200

    basic data completed

--- cycles.cpp
+++ cycles.cpp
@@ -2,6 +2,8 @@
 #include <iomanip>
 #include <fstream>
 #include <string>
+#include <random>
+#include <vector>
 
 using namespace std;
 
@@ -9,7 +11,7 @@ class msasiedztwa
 {
 private:
     int **macierz, msize = 0;
-    void createMatrix(int size)
+    int createMatrix(int size, int filler)
     {
         msize = size;
         macierz = new int *[size];
@@ -18,9 +20,12 @@ private:
             macierz[n] = new int[size];
             for (int m = 0; m < size; m++)
             {
-                macierz[n][m] = 0;
+                macierz[n][m] = filler;
+                if (n == m)
+                    macierz[n][m] = 0;
             }
         }
+        return filler ? 0 : 1;
     }
 
 public:
@@ -40,13 +45,13 @@ public:
         {
             int a, b, validate;
             file >> a >> validate;
-            createMatrix(a);
+            createMatrix(a, 0);
             while (file >> a >> b)
             {
                 a--;
                 b--;
                 macierz[a][b] = 1;
-                macierz[b][a] = -1;
+                macierz[b][a] = 1;
                 validate--;
             }
             if (validate != 0)
@@ -61,8 +66,20 @@ public:
     }
     void generateMatrix(int size, int saturation)
     {
-        createMatrix(size);
-        // TODO generate matrix based on saturation
+        int edges = (((size * (size - 1)) / 2) * (saturation > 50 ? 100 - saturation : saturation)) / 100;
+        int filler = createMatrix(size, saturation > 50);
+        srand((unsigned)time(NULL));
+        while (edges > 0)
+        {
+            int x = rand() % size;
+            int y = rand() % size;
+            if (x != y && macierz[y][x] != filler)
+            {
+                macierz[x][y] = filler;
+                macierz[y][x] = filler;
+                edges--;
+            }
+        }
     }
     void print()
     {
@@ -88,10 +105,97 @@ public:
         }
     }
 };
+class listnastep
+{
+private:
+    vector<vector<int>> lista;
+    void createList(int size)
+    {
+        for (int n = 0; n < size; n++)
+        {
+            lista.push_back(vector<int>());
+        }
+    }
+    bool hasValue(int index, int value)
+    {
+        for (int n = 0; n < lista[index].size(); n++)
+            if (lista[index][n] == value)
+                return true;
+        return false;
+    }
 
-// TODO implement class child list
+public:
+    listnastep(){};
+    ~listnastep()
+    {
+        for (int n = 0; n < lista.size(); n++)
+        {
+            lista[n].clear();
+        }
+        lista.clear();
+    };
+    void loadFromFile(string name = "input.txt")
+    {
+        ifstream file(name);
+        if (file.is_open())
+        {
+            int a, b, validate;
+            file >> a >> validate;
+            createList(a);
+            while (file >> a >> b)
+            {
+                a--;
+                b--;
+                lista[a].push_back(b);
+                validate--;
+            }
+            if (validate != 0)
+            {
+                cout << "[WARN] Incorrect graph." << endl;
+            }
+        }
+        else
+        {
+            cout << "[ERR] File not found." << endl;
+        }
+    }
+    void generateList(int size, int saturation)
+    {
+        int edges = (size * size * saturation) / 100;
+        createList(size);
+        srand((unsigned)time(NULL));
+        while (edges)
+        {
+            int x = rand() % size;
+            int y = rand() % size;
+            if (!hasValue(x, y))
+            {
+                lista[x].push_back(y);
+                edges--;
+            }
+        }
+    }
+    void print()
+    {
+        for (int n = 0; n < lista.size(); n++)
+        {
+            cout << n + 1 << ": ";
+            for (int m = 0; m < lista[n].size(); m++)
+            {
+                cout << setw(3) << lista[n][m] + 1;
+            }
+            cout << endl;
+        }
+    }
+};
 
 int main()
 {
+    // listnastep m;
+    // m.loadFromFile("test.txt");
+    // m.print();
+    msasiedztwa m;
+    m.generateMatrix(5, 60);
+    m.print();
     return 0;
 }
